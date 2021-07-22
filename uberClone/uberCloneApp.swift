e0ccf6743640328baa501dbad5612be35878f397
Bastien <bastiengiat@gmail.com>
2021-07-22 14:41:42 +0200

    Initial Commit

---
+++ uberClone/uberCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  uberCloneApp.swift
+//  uberClone
+//
+//  Created by bastien giat on 22/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct uberCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
